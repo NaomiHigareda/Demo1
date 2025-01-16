46dc37ed8ef90856262e3ae3e22a0e5de59f28af
Naomi Higareda <nfhigareda@actx.edu>
2025-01-16 13:03:21 -0600

    add code to ask name and color

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Demo1
 {
@@ -10,6 +11,11 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Whats your name?");
+
+            string name = Console.ReadLine("What is your favorite color?");
+
+            string color = Console.ReadLine();
         }
     }
 }
